3d2947a861cd09990b26755de4458ed3adbbf03a
Invixor <igor.piratorov@gmail.com>
2023-05-08 15:25:11 +0300

    CV project_08.05.2023

--- CV/script/index.js
+++ CV/script/index.js
@@ -1,16 +1,16 @@
 const tabHeaders = document.querySelectorAll('[data-tab]');
 const headerContent = document.querySelectorAll('[data-tab-content]');
 
-tabHeaders.forEach(function(item){
-    item.addEventListener('click', function(){
+tabHeaders.forEach(item=>{
+    item.addEventListener('click', ()=>{
+        const tabContent = item.nextElementSibling;
+        tabContent.classList.toggle('_hidden');
+    })
+})
 
-        headerContent.forEach(function(item){
-            item.classList.add('_hidden');
-        });
-
-        const contentBox = document.querySelector('#'+this.dataset.tab);
-        contentBox.classList.remove('_hidden');
-    });
-});
-
-
+tabHeaders.forEach(item=>{
+    item.classList.remove('tab_active');
+    item.addEventListener('click', ()=>{
+        item.classList.toggle('tab_active');
+    })
+})
